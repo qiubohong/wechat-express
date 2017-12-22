057bf4cb40dcd3efe3655b5361e0a000490c4749
qborfy <550766314@qq.com>
2017-12-22 17:57:35 +0800

    [note]提交说明

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var app = express();
+var serverPort = 8080;
 
 //初始化全局变量
-(function(){
-	process.env.ACCESSTOKEN = ""; //微信 access_token
-	process.env.JSTICKET = ""; //微信js ticket
+(function() {
+    process.env.ACCESSTOKEN = ""; //微信 access_token
+    process.env.JSTICKET = ""; //微信js ticket
 })();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 
 var tokenRouter = require('./route/token.js')
@@ -17,9 +14,23 @@ app.use('/token', tokenRouter);
 
 app.use(express.static('www'));
 
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(serverPort, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
+var opn = require('opn');
+var os = require('os');
+
+var lacalhost = ''
+try {
+    var network = os.networkInterfaces()
+    localhost = network[Object.keys(network)[0]][1].address
+} catch (e) {
+    localhost = 'localhost';
+}
+var uri = 'http://' + localhost + ':' + serverPort;
+opn(uri);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# 简介
+基于express搭建的微信网页微信授权和网页jssdk使用的一些后端接口。
+
+## 运行
+
+
+## 微信验证接口
+接口名称：`/token/wxJssdk`
+
+## 网页微信授权
+跳转地址接口地址：`/token/wx_login`
+
+## 网页jssdk
+获取配置项：`/token/getWxConfig?url=xxx`
+
+- `url` : 配置地址
